ad62e4ded4f471723c7048b80edc5091019d7c12
赖敬文(LaiJingWen)-顺丰科技技术集团 <01391238@sf.com>
2024-06-16 16:42:28 +0800

    tmp version

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.4
 
 require (
 	github.com/boombuler/barcode v1.0.1
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/tdewolff/canvas v0.0.0-20240521203133-699cfc271500
 )
 
@@ -13,10 +14,11 @@ require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/benoitkugler/textlayout v0.3.0 // indirect
 	github.com/benoitkugler/textprocessing v0.0.3 // indirect
+	github.com/galihrivanto/go-inkscape v0.1.5 // indirect
+	github.com/galihrivanto/runner v0.1.1 // indirect
 	github.com/go-fonts/latin-modern v0.3.1 // indirect
 	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
 	github.com/go-text/typesetting v0.1.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/kolesa-team/go-webp v1.0.4 // indirect
 	github.com/tdewolff/font v0.0.0-20240517133251-c96fa5abd1d4 // indirect
 	github.com/tdewolff/minify/v2 v2.20.5 // indirect
--- main.go
+++ main.go
@@ -13,15 +13,26 @@ import (
 	//canvas
 	"github.com/tdewolff/canvas"
 	"github.com/tdewolff/canvas/renderers"
+	"github.com/tdewolff/canvas/renderers/pdf"
+	//inkscape
 )
 
-var fontLatin *canvas.FontFamily
+var fontDejaVu *canvas.FontFamily
 
-var totalHeight = 200.0
-var y = totalHeight - 10.0
+var X_PACKAGENO = 10.0
+var Y_PACKAGENO = 10.0
 
-func GenQRCode(waybill string, width, height int) image.Image {
-	// Create the barcode
+var X_MARGIN = 12.0
+var Y_MARGIN = 5.0
+
+var QR_SIZE = 33
+var X_QR = 127.0
+var Y_QR = 15.0
+
+var X_CELL = 80.0
+var Y_CELL = 50.0
+
+func GenQRCode(waybill string, destinationCode string, width, height int) image.Image {
 	qrCode, _ := qr.Encode(waybill, qr.M, qr.Auto)
 
 	// Scale the barcode to 200x200 pixels
@@ -57,59 +68,168 @@ func GenBarCode(waybill string, width, height int) image.Image {
 	return code
 }
 
-func drawTextAndMoveDown(c *canvas.Context, x float64, text *canvas.Text) {
-	const spacing = 5
-	y -= text.Bounds().H + spacing
-	c.DrawText(x, y, text)
-}
-
-func renderSFContainerTag(tag string, timestamp string, subWaybillCount int, containerNo string, width, height float64, Orientation int) {
+func renderSFContainerTag(tag string, timestamp string, subWaybillCount int, containerNo, destinationCode string, width, height float64, Orientation int) {
 	cvs := canvas.New(width, height)
 	ctx := canvas.NewContext(cvs)
+	ctx.SetFillColor(canvas.Aqua)
+	ctx.DrawPath(0, 0, canvas.Rectangle(width, height))
 
-	fontLatin = canvas.NewFontFamily("latin")
-	if err := fontLatin.LoadSystemFont("serif", canvas.FontRegular); err != nil {
+	fontDejaVu = canvas.NewFontFamily("dejavu")
+	if err := fontDejaVu.LoadFontFile("./resources/DejaVu_Sans/DejaVuSans.ttf", canvas.FontRegular); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("default y:", y)
-	headerFace := fontLatin.Face(24.0, canvas.Black, canvas.FontBold, canvas.FontNormal)
-	text12Face := fontLatin.Face(12.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
-	text10Face := fontLatin.Face(10.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	leftMargin := 10.0
+	baseMargin := 0.09 * height
+	y := baseMargin
+	Y_MARGIN = 0.08 * height
+	X_MARGIN = 0.08 * width
 
-	headerText := canvas.NewTextBox(headerFace, tag, 0.0, 30.0, canvas.Left, canvas.Top, 0.0, 0.0)
-	drawTextAndMoveDown(ctx, 10.0, headerText)
+	fmt.Println("default y:", y, ", Y_MARGIN:", Y_MARGIN)
+	textTagFace := fontDejaVu.Face(0.24*height, canvas.Black, canvas.FontBold, canvas.FontNormal)
+	textContainerNoFace := fontDejaVu.Face(0.12*height, canvas.Black, canvas.FontBold, canvas.FontNormal)
+	textTimestampFace := fontDejaVu.Face(0.1*height, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	flatTagFace := fontDejaVu.Face(0.24*height, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+
+	//ContainerNo (Bottom-Left)
+	containerNoText := canvas.NewTextBox(textContainerNoFace, containerNo, 0.0, height*0.12, canvas.Left, canvas.Top, 0.0, 0.0)
+	ctx.DrawText(float64(leftMargin), y, containerNoText)
+
+	y += containerNoText.Bounds().H + Y_MARGIN
 	fmt.Println("y update:", y)
 
-	timestamp_and_locate_info := fmt.Sprintf("%s 	(512WE %d ", timestamp, subWaybillCount)
-	timestamp_and_locate_info += "件" + ")"
-	fmt.Println(timestamp_and_locate_info)
-	rt := canvas.NewRichText(text10Face)
-	rt.WriteString(timestamp_and_locate_info)
-	timestampText := rt.ToText(100, 80, canvas.Left, canvas.Right, 0.0, 0.0)
-	drawTextAndMoveDown(ctx, 10.0, timestampText)
-	fmt.Println("y update:", y)
-
-	barcode := GenBarCode(containerNo, 200, 60)
-	y -= 60
-	ctx.DrawImage(10, y, barcode, canvas.DPMM(3.2))
-	y -= float64(barcode.Bounds().Size().Y + 5)
-	fmt.Println("y update:", y)
-
-	containerNoText := canvas.NewTextBox(text12Face, containerNo, 0.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
-	drawTextAndMoveDown(ctx, 10.0, containerNoText)
+	//BarCode
+	barcode := GenBarCode(containerNo, int(width*0.6), int(height*0.3))
+	ctx.DrawImage(leftMargin, y, barcode, canvas.DPMM(1.0))
 
 	//qrcode
-	qrcode := GenQRCode(containerNo, 30, 30)
-	ctx.DrawImage(width-50.0, y, qrcode, canvas.DPMM(3.2))
+	qrCodeX := float64(barcode.Bounds().Max.X) + X_MARGIN
+	qrCodeY := y + (0.25-0.2)*height
+	QR_SIZE = int(height * 0.2)
+	qrcode := GenQRCode(containerNo, destinationCode, QR_SIZE, QR_SIZE)
+	ctx.DrawImage(qrCodeX, float64(qrCodeY), qrcode, canvas.DPMM(1.0))
+
+	//flatTag
+	flatTagX := qrCodeX
+	flatTagW := QR_SIZE
+	flatTagH := QR_SIZE / 2
+	flatTagText := canvas.NewTextBox(flatTagFace, "扁平", float64(flatTagW), float64(flatTagH), canvas.Left, canvas.Top, 0.0, 0.0)
+	flatTagY := qrCodeY - flatTagText.Bounds().H - Y_MARGIN
+
+	ctx.SetStrokeColor(canvas.Black)
+	ctx.SetStrokeWidth(1.0)
+	ctx.DrawPath(flatTagX, flatTagY, canvas.Rectangle(float64(flatTagW), float64(flatTagH)))
+	ctx.Fill()
+
+	ctx.DrawText(flatTagX, flatTagY+flatTagText.Bounds().H, flatTagText)
+
+	//timestamp
+	fmt.Println("barcode.Bounds.Max:", barcode.Bounds().Max, ", barcode.Bounds.Min:", barcode.Bounds().Min, ", diff:", barcode.Bounds().Max.Y-barcode.Bounds().Min.Y)
+	y += float64(barcode.Bounds().Max.Y-barcode.Bounds().Min.Y) + 2*Y_MARGIN
+	fmt.Println("y update:", y)
+
+	timestampLocateInfo := fmt.Sprintf("%s (512WE %d 件)", timestamp, subWaybillCount)
+	fmt.Println(timestampLocateInfo)
+	rt := canvas.NewRichText(textTimestampFace)
+	rt.WriteString(timestampLocateInfo)
+	timestampText := rt.ToText(width, height*0.1, canvas.Left, canvas.Bottom, 0.0, 0.0)
+	ctx.DrawText(leftMargin, y, timestampText)
+
+	//Header
+	headerText := canvas.NewTextBox(textTagFace, tag, 0.0, height*0.2, canvas.Left, canvas.Top, 0.0, 0.0)
+	headerY := height - headerText.Bounds().H
+	ctx.DrawText(float64(leftMargin), headerY, headerText)
 
 	//draw the canvas on the PDF
-	//cvs.RenderTo(pdf)
-	//cvs.RenderTo(canvasWriter)
-	//cvs.WriteFile(containerNo+".pdf", renderers.PNG())
+	renderers.Write(containerNo+".svg", cvs)
+	renderers.Write(containerNo+".png", cvs, canvas.DPI(96.0))
+	renderers.Write(containerNo+".pdf", cvs, &pdf.Options{
+		Compress:      false,
+		SubsetFonts:   false,
+		ImageEncoding: canvas.Lossless,
+	})
+
+	// Options{
+	// 	Compress:      true,
+	// 	SubsetFonts:   true,
+	// 	ImageEncoding: canvas.Lossless,
+	// }
+
+	fmt.Println("Done!!")
+}
+
+func renderTextCoordinate(tag string, timestamp string, subWaybillCount int, containerNo, destinationCode string, width, height float64, Orientation int) {
+	cvs := canvas.New(width, height)
+	ctx := canvas.NewContext(cvs)
+
+	ctx.SetFillColor(canvas.Yellow)
+	ctx.DrawPath(0, 0, canvas.Rectangle(width, height))
+
+	fontDejaVu = canvas.NewFontFamily("latin")
+	if err := fontDejaVu.LoadSystemFont("serif", canvas.FontRegular); err != nil {
+		panic(err)
+	}
+
+	const spacing = 5
+	const margin = 10
+
+	text18Face := fontDejaVu.Face(18.0, canvas.Black, canvas.FontBold, canvas.FontNormal)
+	text12Face := fontDejaVu.Face(12.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+	text10Face := fontDejaVu.Face(10.0, canvas.Green, canvas.FontRegular, canvas.FontNormal)
+	text8Face := fontDejaVu.Face(8.0, canvas.Black, canvas.FontRegular, canvas.FontNormal)
+
+	headerText := canvas.NewTextBox(text18Face, "(10,10)", 0.0, 30.0, canvas.Left, canvas.Top, 0.0, 0.0)
+	face18Height := headerText.Bounds().H
+	fmt.Println("face18Height:", face18Height)
+	ctx.DrawText(margin, margin, headerText)
+
+	containerNoText := canvas.NewTextBox(text12Face, "(width-10,height-10)", 0.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
+	ctx.DrawText(width-margin, height-margin, containerNoText)
+
+	cornerText := canvas.NewTextBox(text10Face, "(width-10,10)", 0.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
+	ctx.DrawText(width-margin, margin, cornerText)
+
+	corner2Text := canvas.NewTextBox(text8Face, "(10,height-10)", 0.0, 0.0, canvas.Left, canvas.Top, 0.0, 0.0)
+	ctx.DrawText(margin, height-margin, corner2Text)
+
+	//ctx.Scale(600.0/width, 400.0/height)
+
+	renderers.Write(containerNo+".svg", cvs)
+	renderers.Write(containerNo+".png", cvs, canvas.DPI(96.0))
 	renderers.Write(containerNo+".pdf", cvs)
 }
 
+func renderElement() {
+	jian := "件"
+	bianPing := "扁平"
+
+	width := 80.0
+	height := 80.0
+
+	cvs := canvas.New(width, height)
+	ctx := canvas.NewContext(cvs)
+
+	fontDejaVu = canvas.NewFontFamily("dejavu")
+	if err := fontDejaVu.LoadFontFile("./resources/DejaVu_Sans/DejaVuSans.ttf", canvas.FontRegular); err != nil {
+		panic(err)
+	}
+
+	textFace := fontDejaVu.Face(24.0, canvas.Black, canvas.FontBold, canvas.FontNormal)
+
+	headerText := canvas.NewTextBox(textFace, jian, width, height, canvas.Left, canvas.Top, 0.0, 0.0)
+	ctx.DrawText(0, 0, headerText)
+
+	renderers.Write("test.svg", cvs)
+	renderers.Write("test.png", cvs, canvas.DPI(96.0))
+
+	fmt.Println(bianPing)
+}
+
 func main() {
-	renderSFContainerTag("123434", "04/23 05:24", 4, "641572254475", 300.0, totalHeight, 1)
+	defaultWidth := 600.0
+	defaultHeight := 400.0
+	//renderElement()
+	renderSFContainerTag("2-13-E-G06", "04/23 05:24", 4, "641572254475", "512W", defaultWidth, defaultHeight, 1)
+
+	//renderTextCoordinate("123434", "04/23 05:24", 4, "11641134343", "512W", defaultWidth, defaultHeight, 1)
 }
